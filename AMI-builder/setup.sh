37869d54b28cbc3c2d3188bedce020486b36b4a0
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-05-28 15:45:49 +0900

    Fix ami setup script

--- AMI-builder/setup.sh
+++ AMI-builder/setup.sh
@@ -18,20 +18,19 @@ update-locale LANG=en_US.UTF-8
 apt-get update
 apt-get -q -y upgrade
 apt-get -q -y install apache2 apt-transport-https openssh-server openssh-client vim jq git \
-                      ca-certificates curl software-properties-common python3-pip figlet \
+                      ca-certificates curl software-properties-common figlet \
                       atop htop make uuid
 
 #Additional modules and config for apache                      
 a2enmod proxy proxy_http rewrite ssl headers
+systemctl restart apache2
 echo "Please wait a few minutes for you instance installation to complete" > /var/www/html/index.html
 
-# Install latest aws cli using pip
-pip3 install -q awscli --upgrade
-export PATH=$PATH:~/.local/bin/
+# Install latest aws cli using snap
+snap install aws-cli --classic
 
 # Install docker
 # Add Docker's official GPG key:
-apt-get -q -y install ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -42,7 +41,6 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get -q -y update
 apt-get -q -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
 usermod -aG docker ubuntu
 
 # Install Certbot
