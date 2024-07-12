56ea14bffa7c801cac96cb93a5465f20944b67d3
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-07-12 14:10:49 +0900

    Update the route53 lambda function to use the v3 SDK and node.js 20.x runtime Package with SAM

--- Lambda/route-53-auto-update/src/handlers/updateRoute53Records.mjs
+++ Lambda/route-53-auto-update/src/handlers/updateRoute53Records.mjs
@@ -1,33 +1,37 @@
+/**
+ * A Lambda function that update route53 records according to ec2 instances state changes
+ */
+
 import { EC2Client, DescribeInstancesCommand } from "@aws-sdk/client-ec2";
 import { Route53Client, ChangeResourceRecordSetsCommand } from "@aws-sdk/client-route-53";
 
 const ec2 = new EC2Client({});
 const route53 = new Route53Client();
 
-export const handler = async (event) => {
+export const updateRoute53Records = async (event, context) => {
+
     console.log('Event:', JSON.stringify(event));
+
+    console.log(`HOST_ZONE_ID: ${process.env.HOSTED_ZONE}`);
     
     const instanceId = event.detail['instance-id'];
+
     console.log('Fetching instance info :', instanceId);
 
-    const describeInstanceResponse = await client.send(new DescribeInstancesCommand({
-        InstanceIds: [
-            instanceId,
-        ]
-    }));
+    const describeInstanceResponse = await ec2.send(new DescribeInstancesCommand(getDescribeInstancePayload(instanceId)));
 
     const instance = describeInstanceResponse.Reservations[0].Instances[0];
-    console.log('Success getting EC2 instance info', JSON.stringify(instance, null, 2));
+    console.log('Success getting EC2 instance info');
 
-    let publicDNS = instance.PublicDnsName;
+    const publicDNS = instance.PublicDnsName;
     console.log('Instance public DNS', publicDNS);
 
     let nuxeoDnsName;
 
-    let templateVer = instance.Tags.find(function (tag) {
+    const templateVer = instance.Tags.find(function (tag) {
         return tag.Key === "cfTemplateVersion";
     });
-    let dnsTag = instance.Tags.find(function (tag) {
+    const dnsTag = instance.Tags.find(function (tag) {
         return tag.Key === "dnsName";
     });
 
@@ -41,6 +45,7 @@ export const handler = async (event) => {
     console.log('Nuxeo DNS Name', nuxeoDnsName);
 
     const instanceState = event.detail['state'];
+    
     let action;
 
     if (instanceState === "running") {
@@ -51,41 +56,59 @@ export const handler = async (event) => {
         throw ("Unsupported instance state");
     }
 
+    //update records
+    const updateRecordResponse = await route53.send(new ChangeResourceRecordSetsCommand(
+        getChangeResourceRecordSetsCommandPayload(action, publicDNS, nuxeoDnsName, !templateVer)
+    ));
+
+    console.log('Success updating route 53 record', JSON.stringify(updateRecordResponse, null, 2));
+
+}
+
+export const getDescribeInstancePayload = instanceId => {
+    return {
+        InstanceIds: [
+            instanceId,
+        ]
+    }
+} 
+
+export const getDnsChangePayload = (action, publicAwsDns, nuxeoDnsName, withKibana) => {
     const dnsChanges = [{
         Action: action,
         ResourceRecordSet: {
-            Name: nuxeoDnsName + '.cloud.nuxeo.com.',
+            Name: nuxeoDnsName + process.env.DOMAIN,
             ResourceRecords: [{
-                Value: publicDNS
+                Value: publicAwsDns
             }],
             TTL: 300,
             Type: "CNAME"
         }
     }];
-    if (!templateVer) {
+    if (withKibana) {
         // Unversioned templates use kibana DNS name
         dnsChanges.push({
             Action: action,
             ResourceRecordSet: {
-                Name: 'kibana-' + nuxeoDnsName + '.cloud.nuxeo.com.',
+                Name: 'kibana-' + nuxeoDnsName + process.env.DOMAIN,
                 ResourceRecords: [{
-                    Value: publicDNS
+                    Value: publicAwsDns
                 }],
                 TTL: 300,
                 Type: "CNAME"
             }
         });
     }
+    return dnsChanges;
+} 
 
-    //update records
-    const updateRecordResponse = await client.send(new ChangeResourceRecordSetsCommand({
+
+export const getChangeResourceRecordSetsCommandPayload = (action, publicAwsDns, nuxeoDnsName, withKibana) => {
+    return {
         ChangeBatch: {
-            Changes: dnsChanges,
-            Comment: "Update after instance restart"
+            Changes: getDnsChangePayload(action, publicAwsDns, nuxeoDnsName, withKibana),
+            Comment: "Update after instance state modification"
         },
         HostedZoneId: process.env.HOSTED_ZONE
-    }));
-
-    console.log('Success updating route 53 record', JSON.stringify(updateRecordResponse, null, 2));
-
-};
+    }
+} 
