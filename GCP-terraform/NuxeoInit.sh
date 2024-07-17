aa9fd0b0d97ad42da2cc1dc501728f75c3a33467
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-07-17 18:27:01 +0900

    WIP

--- GCP-terraform/NuxeoInit.sh
+++ GCP-terraform/NuxeoInit.sh
@@ -70,3 +70,285 @@ git clone ${COMPOSE_REPO} ${COMPOSE_DIR}
 mkdir -p ${CONF_DIR}
 echo "Nuxeo Presales Installation Script: Install Nuxeo => DONE" | tee -a ${INSTALL_LOG}
 
+echo "Nuxeo Presales Installation Script: Configure Nuxeo" | tee -a ${INSTALL_LOG}
+
+# Copy default conf.d files
+cp ${COMPOSE_DIR}/conf.d/* ${CONF_DIR}
+
+BUCKET="nuxeo-demo-shared-bucket-us"
+BUCKET_PREFIX="/${STACK_ID}/"
+
+# Make sure we always have a UI installed
+AUTO_PACKAGES="nuxeo-web-ui gcp-storage platform-explorer"
+
+# Write system configuration
+
+# This is required for WOPI
+JWT_SECRET=`uuid`
+
+cat << EOF > ${CONF_DIR}/system.conf
+# Host Configuration
+session.timeout=600
+nuxeo.url=https://${FQDN}/nuxeo
+
+# Templates
+nuxeo.append.templates.system=${TEMPLATES}
+
+# CORS Configuration (used with AI, Salesforce, others)
+#nuxeo.cors.urls=
+#nuxeo.server.coookies.sameSite=none
+
+# Webui
+nuxeo.selection.selectAllEnabled=true
+nuxeo.analytics.documentDistribution.disableThreshold=10000
+
+# Mail Configuration
+mail.transport.password=${MAIL_PASS}
+mail.transport.host=smtp.mail.us-east-1.awsapps.com
+mail.transport.port=465
+mail.transport.user=no-reply@nuxeo-demo.awsapps.com
+mail.transport.auth=true
+mail.from=no-reply@nuxeo-demo.awsapps.com
+mail.smtp.ssl.enable=true
+mail.transport.protocol=smtps
+mail.transport.ssl.protocol=TLSv1.2
+nuxeo.notification.eMailSubjectPrefix=[Nuxeo]
+
+# BLOB Configuration
+nuxeo.gcp.storage.bucket=${BUCKET}
+nuxeo.gcp.storage.bucket_prefix=${BUCKET_PREFIX}
+nuxeo.gcp.project=nuxeo-presales-apis
+
+# WOPI Configuration
+nuxeo.wopi.discoveryURL=https://onenote.officeapps.live.com/hosting/discovery
+nuxeo.wopi.baseURL=https://wopi.nuxeocloud.com/${FQDN}/nuxeo/
+# JWT token is required for WOPI
+nuxeo.jwt.secret=${JWT_SECRET}
+
+EOF
+
+# Register the nuxeo instance
+echo "$(date) Configure Studio Project [${NX_STUDIO}]" | tee -a ${INSTALL_LOG}
+
+# Home required by 'docker'
+export HOME="/home/ubuntu"
+
+# Get credentials for Studio & Repository
+gcloud secrets versions access latest --secret nuxeo-presales-connect --project nuxeo-presales-apis > /root/creds.json
+
+# Log in to docker
+DOCKER_USER=$(jq -r '.docker_presales_user' < /root/creds.json)
+DOCKER_PASS=$(jq -r '.docker_presales_pwd' < /root/creds.json)
+echo ${DOCKER_PASS} | docker login -u ${DOCKER_USER} --password-stdin docker-private.packages.nuxeo.com 2>&1 | tee -a ${INSTALL_LOG}
+
+STUDIO_PACKAGE=""
+if [ -z "${NX_STUDIO_VER}" ]; then
+  NX_STUDIO_VER="0.0.0-SNAPSHOT"
+fi
+if [ -n "${NX_STUDIO}" ]; then
+  STUDIO_PACKAGE="${NX_STUDIO}-${NX_STUDIO_VER}"
+fi
+PROJECT_NAME=$(echo "${NX_STUDIO}" | awk '{print tolower($0)}')
+
+# Set working environment
+CREDENTIALS=$(jq -r '.connect_presales_pwd' < /root/creds.json)
+cat << EOF > ${NUXEO_ENV}
+APPLICATION_NAME=${NX_STUDIO}
+PROJECT_NAME=${PROJECT_NAME}
+
+NUXEO_IMAGE=${FROM_IMAGE}
+
+JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8787
+
+CONNECT_URL=https://connect.nuxeo.com/nuxeo/site/
+
+NUXEO_DEV=true
+NUXEO_PORT=8080
+NUXEO_PACKAGES=${STUDIO_PACKAGE} ${AUTO_PACKAGES} ${NUXEO_PACKAGES}
+
+INSTALL_RPM=${INSTALL_RPM}
+
+MONGO_VERSION=${MONGO_VERSION}
+OPENSEARCH_IMAGE=${OPENSEARCH_IMAGE}
+OPENSEARCH_DASHBOARDS_IMAGE=${OPENSEARCH_DASHBOARDS_IMAGE}
+
+FQDN=${FQDN}
+STUDIO_USERNAME=${STUDIO_USERNAME}
+STUDIO_CREDENTIALS=${CREDENTIALS}
+EOF
+# Make env not as hidden
+ln -s ${NUXEO_ENV} ${COMPOSE_DIR}/env
+
+# Fix up permissions
+rm -f /root/creds.json
+chown -R nuxeo:ubuntu ${TMP_DIR}
+chown -R ubuntu:ubuntu ${COMPOSE_DIR} ${HOME}/.docker
+
+# Use the source image to register the project
+docker pull --quiet ${FROM_IMAGE} 2>&1 | tee -a ${INSTALL_LOG}
+
+# Auto-start if Studio project defined
+if [ -n "${NX_STUDIO}" ]; then
+  echo "Registering Nuxeo..." | tee -a ${INSTALL_LOG}
+  sh -c "cd ${COMPOSE_DIR} && ./generate_clid.sh"
+
+  # Build / Pull images
+  echo "Pulling other images..." | tee -a ${INSTALL_LOG}
+  docker compose --project-directory ${COMPOSE_DIR} --no-ansi pull | tee -a ${INSTALL_LOG}
+
+  if [[ "${AUTO_START}" == "true" ]]; then
+    echo "Building images..." | tee -a ${INSTALL_LOG}
+    docker compose --project-directory ${COMPOSE_DIR} --no-ansi build --progress plain 2>&1 | tee -a ${INSTALL_LOG}
+
+    echo "Starting Nuxeo stack" | tee -a ${INSTALL_LOG}
+    docker compose --project-directory ${COMPOSE_DIR} --no-ansi up --detach --no-color 2>&1 | tee -a ${INSTALL_LOG}
+  fi
+
+  # Fix up permissions
+  chown -R ubuntu:ubuntu ${HOME}/.docker
+
+fi
+echo "Nuxeo Presales Installation Script: Configure Nuxeo => DONE" | tee -a ${INSTALL_LOG}
+
+echo "Nuxeo Presales Installation Script: Install Misc." | tee -a ${INSTALL_LOG}
+# Update some defaults
+update-alternatives --set editor /usr/bin/vim.basic
+echo "Nuxeo Presales Installation Script: Install Misc. => DONE" | tee -a ${INSTALL_LOG}
+
+# Configure reverse-proxy
+cat << EOF > /etc/apache2/sites-available/nuxeo.conf
+<VirtualHost _default_:80>
+    ServerName  ${FQDN}
+    CustomLog /var/log/apache2/nuxeo_access.log combined
+    ErrorLog /var/log/apache2/nuxeo_error.log
+    Redirect permanent / https://${FQDN}/
+</VirtualHost>
+
+<VirtualHost _default_:443 >
+
+    ServerName  ${FQDN}
+
+    CustomLog /var/log/apache2/nuxeo_access.log combined
+    ErrorLog /var/log/apache2/nuxeo_error.log
+
+    DocumentRoot /var/www
+
+    ProxyRequests   Off
+     <Proxy * >
+        Order allow,deny
+        Allow from all
+     </Proxy>
+
+    <Location /dashboards>
+      AuthUserFile /etc/apache2/passwords
+      AuthName authorization
+      AuthType Basic
+      require valid-user
+    </Location>
+
+    RewriteEngine   On
+    RewriteRule ^/$ /nuxeo/ [R,L]
+    RewriteRule ^/nuxeo$ /nuxeo/ [R,L]
+    RewriteRule ^/dashboards$ /dashboards/ [R,L]
+
+    ProxyPass           /nuxeo/         http://localhost:8080/nuxeo/
+    ProxyPass           /dashboards/        http://localhost:5601/dashboards/
+    ProxyPassReverse    /nuxeo/         http://localhost:8080/nuxeo/
+    ProxyPassReverse    /dashboards/        http://localhost:5601/dashboards/
+    ProxyPreserveHost   On
+
+    # WSS
+    ProxyPass         /_vti_bin/     http://localhost:8080/_vti_bin/
+    ProxyPass         /_vti_inf.html http://localhost:8080/_vti_inf.html
+    ProxyPassReverse  /_vti_bin/     http://localhost:8080/_vti_bin/
+    ProxyPassReverse  /_vti_inf.html http://localhost:8080/_vti_inf.html
+
+    RequestHeader   append nuxeo-virtual-host "https://${FQDN}/"
+
+    # Retain TLS1.1 for backwards compatibility until Jan 2020
+    # These must be *after* the Certbot entry
+    #XXX SSLProtocol all -SSLv2 -SSLv3 -TLSv1
+    # SSLProtocol all -SSLv2 -SSLv3 -TLSv1 -TLSv1.1
+    # Enable high ciphers for 3rd party security scanners
+    #XXX SSLCipherSuite HIGH:!aNULL:!MD5:!3DES
+
+    ## BEGIN SUPINT-655 ##
+    <Location "/nuxeo/incl">
+      RewriteRule .* - [R=404,L,NC]
+    </Location>
+    ## END SUPINT-655 ##
+
+    Header edit Set-Cookie ^(.*)$ \$1;SameSite=None;Secure
+
+</VirtualHost>
+EOF
+
+# Add gzip compression for the REST API
+cat > /etc/apache2/mods-available/deflate.conf <<EOF
+<IfModule mod_deflate.c>
+        <IfModule mod_filter.c>
+                # these are known to be safe with MSIE 6
+                AddOutputFilterByType DEFLATE text/html text/plain text/xml
+
+                # everything else may cause problems with MSIE 6
+                AddOutputFilterByType DEFLATE text/css
+                AddOutputFilterByType DEFLATE application/x-javascript application/javascript application/ecmascript
+                AddOutputFilterByType DEFLATE application/rss+xml
+                AddOutputFilterByType DEFLATE application/xml
+                AddOutputFilterByType DEFLATE application/json
+        </IfModule>
+</IfModule>
+EOF
+
+a2enmod proxy proxy_http rewrite ssl headers
+a2dissite 000-default
+a2ensite nuxeo
+
+#KIBANA_PASS=$(aws secretsmanager get-secret-value --secret-id kibana_default_password --region us-west-2 | jq -r '.SecretString|fromjson|.kibana_default_password')
+#htpasswd -b -c /etc/apache2/passwords kibana "${KIBANA_PASS}"
+#apache2ctl -k graceful
+
+# Restart apache
+echo "Restarting Apache"
+systemctl restart apache2
+
+# Enable SSL certs
+echo "Nuxeo Presales Installation Script: Enable Certbot Certificate" | tee -a ${INSTALL_LOG}
+certbot -q --apache --redirect --hsts --uir --agree-tos -m wwpresalesdemos@hyland.com -d ${FQDN} | tee -a ${INSTALL_LOG}
+
+echo "Nuxeo Presales Installation Script: Setup profile, ubuntu, etc." | tee -a ${INSTALL_LOG}
+
+#set up ubuntu user
+cat << EOF >> /home/ubuntu/.profile
+export TERM="xterm-color"
+export PS1='\[\e[0;33m\]\u\[\e[0m\]@\[\e[0;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w\[\e[0m\]\$ '
+export COMPOSE_DIR=${COMPOSE_DIR}
+alias dir='ls -alFGh'
+alias hs='history'
+alias mytail='nxl'
+alias vilog='stack vilog'
+alias mydu='du -sh */'
+
+# Add stack management and QOL aliases
+source ${COMPOSE_DIR}/aliases.sh
+
+# Override some of the above for CLOUD usage
+alias stack='make -e -f ${COMPOSE_DIR}/Makefile'
+
+# Extras for CLOUD usage
+alias nxenv='vim ${COMPOSE_DIR}/.env'
+alias nxconf='vim ${CONF_DIR}/system.conf'
+
+figlet $FQDN
+EOF
+
+# Set up vim for ubuntu user
+cat << EOF > /home/ubuntu/.vimrc
+" Set the filetype based on the file's extension, but only if
+" 'filetype' has not already been set
+au BufRead,BufNewFile *.conf setfiletype conf
+EOF
+echo "Nuxeo Presales Installation Script: Setup profile, ubuntu, etc. => DONE" | tee -a ${INSTALL_LOG}
+
+echo "Nuxeo Presales Installation Script Complete" | tee -a ${INSTALL_LOG}
+
--- GCP-terraform/main.tf
+++ GCP-terraform/main.tf
@@ -5,11 +5,25 @@ variable "stack_name" {
   description = "The name of the demo stack"
 }
 
-resource "google_compute_instance" "nuxeoinstance" {
+resource "google_compute_instance" "nuxeo_instance" {
   project      = "nuxeo-presales-apis"
   name         = "${var.stack_name}-instance"
   machine_type = "e2-standard-2"
   zone         = "us-central1-a"
+  service_account {
+    email = "1007087250969-compute@developer.gserviceaccount.com"
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/pubsub",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+      "storage-full",
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
   metadata = {
     enable-oslogin : "TRUE"
     stack-name : var.stack_name
