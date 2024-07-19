20c9a4090b7dcafbe991c1e8806dd592de148668
Josh Fletcher <josh.fletcher@hyland.com>
2024-07-19 15:44:57 -0700

    Add params for separate DNS name and auto-start.

--- GCP-terraform/main.tf
+++ GCP-terraform/main.tf
@@ -1,8 +1,19 @@
-# Create a VM instance from the nuxeo image
+# Create Nuxeo stack on GCP using Terraform.
 
 variable "stack_name" {
   type        = string
-  description = "Stack name (used for Compute Instance Name and DNS if not set)"
+  description = "Stack name (used for Compute Instance Name and DNS if not set)."
+}
+
+variable "dns_name" {
+  type = string
+  description = "DNS name (i.e. dns_name.gcp.cloud.nuxeo.com)."
+  default = ""
+}
+
+# DNS name defaults to stack_name if nothing is set
+locals {
+  dns_name = var.dns_name == "" ? var.stack_name : var.dns_name
 }
 
 variable "nx_studio" {
@@ -14,6 +25,12 @@ variable "with_nev" {
   type        = bool
   description = "Deploy NEV? [true|false]"
   default     = false
+}
+
+variable "auto_start" {
+  type        = bool
+  description = "Automatically start Nuxeo?"
+  default     = true
 }
 
 # Nuxeo Instance resources
@@ -47,10 +64,11 @@ resource "google_compute_instance" "nuxeo_instance" {
   metadata = {
     enable-oslogin : "TRUE"
     stack-name : var.stack_name
+    dns-name: local.dns_name
     nx-studio: var.nx_studio
     with-nev: var.with_nev
     nuxeo-secret: random_password.nuxeo_secret.result
-    auto-start: "true"
+    auto-start: var.auto_start
     startup-script: file("./files/NuxeoInit.sh")
   }
   tags = ["http-server","https-server"]
@@ -70,7 +88,7 @@ resource "google_compute_instance" "nuxeo_instance" {
 resource "google_dns_record_set" "nuxeo_instance_dns_record" {
   project      = "nuxeo-presales-apis"
   managed_zone = "gcp"
-  name         = "${var.stack_name}.gcp.cloud.nuxeo.com."
+  name         = "${local.dns_name}.gcp.cloud.nuxeo.com."
   type         = "A"
   rrdatas      = ["${google_compute_instance.nuxeo_instance.network_interface.0.access_config.0.nat_ip}"]
   ttl          = 300
@@ -81,6 +99,7 @@ module "nev" {
   count = var.with_nev ? 1 : 0
   source = "./modules/nev"
   stack_name = "${var.stack_name}-nev"
-  nuxeo_url = "https://${var.stack_name}.gcp.cloud.nuxeo.com"
+  dns_name = "${local.dns_name}-nev"
+  nuxeo_url = "https://${local.dns_name}.gcp.cloud.nuxeo.com"
   nuxeo_secret= random_password.nuxeo_secret.result
 }
